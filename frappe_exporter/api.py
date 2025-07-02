bdf1f4b447c8d179e0e9c1511d9b06c16066f7af
Prateek Jayaswal <jayaswalprateek@gmail.com>
2025-07-02 13:37:09 +0530

    feat: add method to get custom doctypes for selection

--- frappe_exporter/api.py
+++ frappe_exporter/api.py
@@ -11,11 +11,27 @@ def metrics():
 
     # Create a Werkzeug Response object
     response = Response(
-        response=prometheus_data_bytes,  # This should be bytes or a string
+        response=prometheus_data_bytes,
         status=200,
-        mimetype="text/plain; version=0.0.4; charset=utf-8",  # Use mimetype for Werkzeug Response
+        mimetype="text/plain; version=0.0.4; charset=utf-8",
     )
 
     # Return this Werkzeug Response object directly.
     # Frappe's API handler will recognize this and use it as-is.
     return response
+
+
+@frappe.whitelist()
+def get_custom_doctypes():
+    # Frappe and ERPNext are considered core apps. You can add more to this list if needed.
+    core_modules = frappe.get_all(
+        "Module Def", filters={"app_name": ("in", ["frappe", "erpnext"])}, pluck="name"
+    )
+
+    custom_doctypes = frappe.get_all(
+        "DocType",
+        filters={"custom": 0, "issingle": 0, "module": ("not in", core_modules)},
+        fields=["name"],
+        order_by="name",
+    )
+    return [d.name for d in custom_doctypes]
